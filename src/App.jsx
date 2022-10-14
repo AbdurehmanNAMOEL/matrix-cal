43552474473cd80fab4e911d7517f51c7fb98ba0
Abdurehman Saeed <asaeed526@gmail.com>
2022-10-14 13:07:43 +0300

    matrix logic edited

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,8 @@ const [row,setRaw]=useState(0)
 const [column,setColumn]=useState(0)
 const [inputValue,setValue]= useState([])
 
-const handleTestEvent=(e)=>{
-     console.log(e.target.offsetTop);
-}
+const handleTestEvent=(e)=> console.log(e.target.offsetTop);
+
 
 const createDummyMatrix=()=>{
  dummyArray = Array(row).fill(new Array(column).fill(0))
@@ -23,55 +22,84 @@ const createDummyMatrix=()=>{
 
 let newArray2=
 [
- [2,3,4,5,8,1],
+
  [1,5,6,7,0,1],
- [9,7,0,1,5,1],
- [3,2,0,1,4,12],
- [3,2,0,1,2,11],
- [3,2,0,1,2,20]
+ [9,7,3,1,5,1],
+ [3,2,5,1,4,12],
+ [3,2,6,1,2,11],
+ [3,2,8,1,2,20]
 ]
 
 let matrix=[]
 let subMatrix=[]
 let combianArray2=[]
+let dete=[]
  
 // calculating the determinant
 
- const genericMatrixCreator=(inputMatrix)=>{
+ const determinantOf2x2Matrix=(inputMatrix)=>{
      return (inputMatrix[0][0]*inputMatrix[1][1])-(inputMatrix[0][1]*inputMatrix[1][0])
 }
 
+let count=0
 // getting the subMatrix from the big matrix
 
- const getSubMatrix=(inputMatrix)=>{
-  if(inputMatrix.length===2){
-    genericMatrixCreator(inputMatrix)
-  }else if(inputMatrix.length>2){
-    subMatrix2(inputMatrix)
-  } 
-  if(subMatrix.length>2){
-    inputMatrix=subMatrix
-    subMatrix=[]
-    getSubMatrix(inputMatrix)
-  }
-   
-}
 
+let determinant=0
+const finalDeterimenant=()=>{
+  for(let i=0;i<dete.length;i++){
+    determinant+=Math.pow(-1,i)*dete[i]
+  }
+  console.log(determinant)
+}
 const subMatrix2=(matrixInput)=>{
    for(let i=1;i<matrixInput.length;i++){
-        for(let j=1;j<matrixInput.length;j++){
-           matrix.push(matrixInput[i][j])
-        }
+      for(let j=0;j<matrixInput.length;j++){
+        if(i!==0){
+          if(j!==count){
+             matrix.push(Math.pow(-1,count)*matrixInput[0][0]*matrixInput[i][j])
+          }
+          console.log(count);
+        } else break  
+               
+      }     
   subMatrix.push(matrix.splice(0,matrixInput.length))
-  }
-   console.log('submatrix',subMatrix,'length',subMatrix.length);
-   if(subMatrix.length===2){
-    console.log(`The Determinant is |A|= ${genericMatrixCreator(subMatrix)}`);
-   }
-   matrix=[]
 }
 
-getSubMatrix(newArray2)
+ if(subMatrix.length>2){
+   console.log(subMatrix,subMatrix.length);
+  matrixInput=subMatrix
+  subMatrix=[]
+  subMatrix2(matrixInput)
+}else if(subMatrix.length===2){
+  console.log(subMatrix);
+  console.log(`The determinant of |A| is : ${determinantOf2x2Matrix(subMatrix)}`);
+  dete.push(determinantOf2x2Matrix(subMatrix))
+  
+   subMatrix=[]
+   if(count!==newArray2.length){
+    count++
+    subMatrix2(newArray2)
+   }else  finalDeterimenant()
+ 
+}
+
+ 
+    
+  
+   console.log('sub-matrix',subMatrix,'length',subMatrix.length);
+  //  if(subMatrix.length===2){
+  //   console.log(`The Determinant is |A|= ${genericMatrixCreator(subMatrix)}`);
+  //  }else if(subMatrix.length>2){
+  //   matrixInput=subMatrix
+  //   subMatrix=[]
+  //   subMatrix2(matrixInput)
+  // }
+ 
+}
+
+subMatrix2(newArray2)
+
 
 const arrayNew=[]
 
@@ -96,14 +124,13 @@ const transposeOfMatrix=(inputMatrix)=>{
       inputMatrix[i][j]=inputMatrix[j][i]
     } 
   }
-
   console.log(inputMatrix);
 }
 
 const getProductOfMatrix=(m1,m2)=>{
      
      if(m1.length!==m2.length){
-           console.log('you need square matrix');
+        console.log('you need square matrix');
      }else{
      for(let i=0;i<m1.length;i++){
       for(let j=0;j<m1.length;j++){
